41846967f41bb81cfd177d67938da7b37a364773
Tam Le <letam210795@gmail.com>
2022-08-18 18:18:13 +0700

    Add product of array except self solution

---
+++ src/com/ntcode/java/array/ProductOfArrayExceptSelf.java
@@ -0,0 +1,26 @@
+package com.ntcode.java.array;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+    public static void main(String[] args) {
+        int[] ex1 = new int[]{1, 2, 3, 4};
+        int[] ex2 = new int[]{-1, 1, 0, -3, 3};
+        System.out.println(Arrays.toString(productExceptSelf(ex1)));
+        System.out.println(Arrays.toString(productExceptSelf(ex2)));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] arr = new int[nums.length];
+        int right = 1, left = 1;
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = left;
+            left *= nums[i];
+        }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            arr[i] *= right;
+            right *= nums[i];
+        }
+        return arr;
+    }
+}
