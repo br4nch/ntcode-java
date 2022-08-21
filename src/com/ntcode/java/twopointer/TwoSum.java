cc2b273dcffbe0d15a4c00429ab01ab60c544831
Tam Le <letam210795@gmail.com>
2022-08-21 13:40:32 +0700

    Add solution for valid palindrome

--- src/com/ntcode/java/twopointer/TwoSum.java
+++ src/com/ntcode/java/twopointer/TwoSum.java
@@ -1,4 +1,4 @@
-package com.ntcode.java.array;
+package com.ntcode.java.twopointer;
 
 import java.util.Arrays;
 import java.util.HashMap;
---
+++ src/com/ntcode/java/twopointer/ValidPalindrome.java
@@ -0,0 +1,53 @@
+package com.ntcode.java.twopointer;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String ex1 = "A man, a plan, a canal: Panama";
+        String ex2 = "race a car";
+        System.out.println(isPalindrome(ex1));
+        System.out.println(isPalindrome(ex2));
+    }
+
+    public static boolean isPalindrome(String s) {
+        char[] arr = s.toLowerCase().replaceAll("[^A-Za-z0-9]", "").toCharArray();
+        int last = arr.length - 1;
+        int mid = arr.length / 2;
+
+        for (int i = 0; i < mid; i++) {
+            if (arr[i] != arr[last]) {
+                return false;
+            }
+            last--;
+        }
+        return true;
+    }
+
+    public static boolean isPalindromeV2(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+
+            char start = s.charAt(i);
+            char end = s.charAt(j);
+
+            if (!Character.isLetterOrDigit(start)) {
+                i++;
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(end)) {
+                j--;
+                continue;
+            }
+
+            if (Character.toLowerCase(start) != Character.toLowerCase(end)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
