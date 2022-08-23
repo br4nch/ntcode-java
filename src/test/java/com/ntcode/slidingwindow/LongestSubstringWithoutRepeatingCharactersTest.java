a98dfd242a18f2c9f9599ef6494c276afce8641d
Tam Le <letam210795@gmail.com>
2022-08-23 14:49:38 +0700

    Add solution for longest substring

---
+++ src/test/java/com/ntcode/slidingwindow/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -0,0 +1,18 @@
+package com.ntcode.slidingwindow;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class LongestSubstringWithoutRepeatingCharactersTest {
+
+    private final LongestSubstringWithoutRepeatingCharacters main = new LongestSubstringWithoutRepeatingCharacters();
+
+    @Test
+    void execute() {
+        var input = "abcabccbb";
+        int expected = 3;
+        int res = main.execute(input);
+
+        assertEquals(expected, res);
+    }
+}
