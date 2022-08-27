a695e05702f2adb400bea3baf280570320b25969
Tam Le <letam210795@gmail.com>
2022-08-27 19:10:36 +0700

    Update solution for PermutationInString

--- src/main/java/com/ntcode/slidingwindow/PermutationInString.java
+++ src/main/java/com/ntcode/slidingwindow/PermutationInString.java
@@ -3,6 +3,32 @@ package com.ntcode.slidingwindow;
 public class PermutationInString {
     public boolean checkInclusion(String s1, String s2) {
 
-        return false;
+        if (s2.length() < s1.length()) return false;
+        int[] arr = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            arr[s1.charAt(i) - 'a']++;
+        }
+        int i = 0;
+        int j = 0;
+        for (; j < s1.length(); j++) {
+            arr[s2.charAt(j) - 'a']--;
+        }
+        j--;
+        if (isEmpty(arr)) return true;
+        while (j < s2.length()) {
+            arr[s2.charAt(i) - 'a']++;
+            i++;
+            j++;
+            if (j < s2.length()) arr[s2.charAt(j) - 'a']--;
+            if (isEmpty(arr)) return true;
+        }
+        return isEmpty(arr);
+    }
+
+    private boolean isEmpty(int[] arr) {
+        for (int j : arr) {
+            if (j != 0) return false;
+        }
+        return true;
     }
 }
